7139d462806c4c161a7445b7acba0052126cc2ac
Greg Griffith <griffith_g@hotmail.com>
2022-11-19 18:28:31 -0500

    printed current weather info to console

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var apiKey = "608e89a8abd53641f8198304f956f5a7";
 var city = "toronto";
 
-var queryWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-var queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+var queryWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey+"&units=metric";
+var queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey+"&units=metric";
 
 fetch(queryWeatherURL)
     .then(function (response) {
@@ -10,4 +10,9 @@ fetch(queryWeatherURL)
     })
     .then(function (data) {
     console.log(data)
+    console.log("Temp: " + (data.main.temp) + "C")
+    console.log("Wind: " + (data.wind.speed/10*36) + "Km/h")
+    console.log("Humidity: " + (data.main.humidity) + "%")
     })
+
+ 
